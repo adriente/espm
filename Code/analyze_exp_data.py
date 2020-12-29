9144cd29a668cde921514df61be491b813124123
adriente <adrien.teurtrie@u-psud.fr>
2020-12-29 16:23:11 +0100

    scale retrieved from data file

--- Code/analyze_exp_data.py
+++ Code/analyze_exp_data.py
@@ -47,7 +47,7 @@ tol = json_dict["tol"]
 max_iter = json_dict["max_iter"]
 b_tol = json_dict["b_tol"]
 phases = json_dict["phases"]
-em = EDXS_model.EDXS_Model(json_dict["paths"]["xray_db"])
+em = EDXS_model.EDXS_Model(json_dict["paths"]["xray_db"],e_offset=S.axes_manager[2].offset, e_scale=S.axes_manager[2].scale, e_size= S.axes_manager[2].size)
 em.generate_g_matr(json_dict["elements_list"])
 
 # If mu_sparse !=0 a good initialization of the first phase is required, it can be done using the spectrum below
