9bb70b8a6e5320023cf586eff385f6aa3ed7e161
Nathanaël Perraudin <6399466+nperraud@users.noreply.github.com>
2021-03-11 18:44:56 +0100

    fix some bugs

--- scripts/generate_synthetic_dataset.py
+++ scripts/generate_synthetic_dataset.py
@@ -1,5 +1,5 @@
-import snmfem.generate_data as gd
-import snmfem.EDXS_model as em
+from snmfem import generate_data as gd
+from snmfem.models import EDXS
 import numpy as np
 from snmfem.conf import DB_PATH, DATASETS_PATH
 from pathlib import Path
@@ -13,29 +13,28 @@ def generate_synthetic_dataset(seeds=[0], N=100):
     seeds: (List[Int]) list of the seeds for the dataset
     N: (Int) Average number of counts in one spectrum of the artificial data
     """
-    abs_db_path = None
-    #abs_db_path = "Data/wernisch_abs.json"
-    abs_elt_dict = None
-
     # Continuum X-rays parameters
     # They were determine by fitting experimental data from 0.6 to 18 keV. Since low energies 
     # were incorporated, the model is only effective and not quantitative.
     brstlg_pars = {"c0": 0.094, "c1": 1417, "c2": 1e-6,
                    "b0": 1.2, "b1": -0.06, "b2": 0.00683}
-
     scale = 1
+    e_scale = 0.01
+    e_offset = 0.208
+    e_size = 1980
+    db_name = "simple_xrays_threshold.json"
 
     # Creation of the pure spectra of the different phases.
-    phase1 = em.EDXS_Model(DB_PATH, abs_db_path, brstlg_pars)
+    phase1 = EDXS(e_offset,e_size,e_scale,brstlg_pars,db_name)
     # Gaussians corresponding to elements
     phase1.generate_spectrum({"8": 1.0, "12": 0.51, "14": 0.61, "13": 0.07, "20": 0.04,
                               "62": 0.02, "26": 0.028, "60": 0.002, "71": 0.003, "72": 0.003, "29": 0.02}, scale)
 
-    phase2 = em.EDXS_Model(DB_PATH, abs_db_path, brstlg_pars)
+    phase2 = EDXS(e_offset,e_size,e_scale,brstlg_pars,db_name)
     phase2.generate_spectrum({"8": 0.54, "26": 0.15, "12": 1.0, "29": 0.038,
                               "92": 0.0052, "60": 0.004, "31": 0.03, "71": 0.003}, scale)
 
-    phase3 = em.EDXS_Model(DB_PATH, abs_db_path, brstlg_pars)
+    phase3 = EDXS(e_offset,e_size,e_scale,brstlg_pars,db_name)
     phase3.generate_spectrum({"8": 1.0, "14": 0.12, "13": 0.18, "20": 0.47,
                               "62": 0.04, "26": 0.004, "60": 0.008, "72": 0.004, "29": 0.01}, scale)
 
@@ -44,15 +43,23 @@ def generate_synthetic_dataset(seeds=[0], N=100):
     phases = np.array([phase1.spectrum, phase2.spectrum, phase3.spectrum])
     # list of densities which will give different total number of events per spectra
     densities = np.array([1.0, 1.33, 1.25])
-
-    spim = gd.AritificialSpim(phases, densities, (80, 80))
-
-    spim.sphere((25, 30), 3.5, 3.5, 0.0, 0.5, 1)
-    spim.sphere((55, 30), 3.5, 3.5, 0.0, 0.5, 2)
-    folder = DATASETS_PATH / Path("aspim037_N{}_2ptcls_brstlg".format(N))
-    folder.mkdir(exist_ok=True, parents=True)
-
     for seed in tqdm(seeds):
+        spim = gd.ArtificialSpim(phases, densities, (80, 80))
+
+        if seed == 0:
+            spim.sphere((25, 30), 3.5, 3.5, 0.0, 0.5, 1)
+            spim.sphere((55, 30), 3.5, 3.5, 0.0, 0.5, 2)
+        else:
+            np.random.seed(seed)
+            for _ in range(2):
+                p = np.random.randint(1, 80, [2])
+                spim.sphere(p, 3.5, 3.5, 0.0, 0.5, 1)            
+
+        
+        folder = DATASETS_PATH / Path("aspim037_N{}_2ptcls_brstlg".format(N))
+        folder.mkdir(exist_ok=True, parents=True)
+
+    
         filename = folder / Path("sample_{}".format(seed))
         spim.generate_spim_stochastic(N, seed=seed)
         spim.save(filename=filename)
