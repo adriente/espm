cae267aafa693c3aadf23bad03a4ec37e588ae22
adriente <adrien.teurtrie@epfl.ch>
2021-05-06 15:49:48 +0200

    trying to fix tests

--- snmfem/tests/test_experiments.py
+++ snmfem/tests/test_experiments.py
@@ -45,6 +45,7 @@ def test_load_data() :
     samples , k  = exps.load_samples(script_file, base_path_conf=base_path, base_path_dataset=base_path)
     for r in range(params["seeds_range"]):
         Xflat, true_spectra_flat, true_maps_flat, G, shape_2d = exps.load_data(samples[r])
+        print(Xflat.shape)
         
         data_path = folder / Path("sample_{}.npz".format(r))
         data = np.load(data_path)
@@ -52,7 +53,7 @@ def test_load_data() :
         Xflat_test = (data["X"].reshape(shape_2d_test[0]*shape_2d_test[1],params["model_parameters"]["e_size"])).T
         true_spectra_test = data["phases"]*params["N"]*np.array(params["densities"])[:,np.newaxis]
         true_maps_test = (data["weights"].reshape(shape_2d_test[0]*shape_2d_test[1],k)).T
-
+        print(Xflat_test.shape, data["X"].shape, shape_2d)
         assert(tuple(shape_2d) == shape_2d_test)
         np.testing.assert_array_equal(Xflat, Xflat_test)
         np.testing.assert_array_equal(data["G"],G)
