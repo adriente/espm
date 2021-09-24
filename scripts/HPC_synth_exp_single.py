cdf1a5e8cfc16c11bbcb43fc361b72dedae2c75e
adriente <adrien.teurtrie@epfl.ch>
2021-09-24 17:46:02 +0200

    HPC error fix

--- scripts/HPC_synth_exp_single.py
+++ scripts/HPC_synth_exp_single.py
@@ -8,7 +8,7 @@ def run_single_exp(pos_dict,est_dict,eval_dict) :
     exp = e.build_exp(pos_dict, est_dict)
     spim, estim = e.quick_load(exp,P_type = fixed_P, simulated=True)
     metrics, mat_tuple, losses = e.run_experiment(spim, estim, exp, simulated=True)
-    e.store_in_file(eval_dict["file"], metrics, mat_tuple, losses)
+    e.store_in_file(eval_dict["output_file"], metrics, mat_tuple, losses)
 
 if __name__ == "__main__" : 
     print(sys.argv[1:])
