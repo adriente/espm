correct formula for P to abs coeff

snmfem/models/EDXS_function.py
@@ -3,6 +3,10 @@ from scipy.special import erfc
 from snmfem.models.absorption_edxs import det_efficiency_from_curve,det_efficiency,absorption_correction
 from snmfem.models import edxs as e
 from collections import Counter
+from snmfem.conf import SYMBOLS_PERIODIC_TABLE
+import json
+
+from snmfem.utils import number_to_symbol_list
     
 def gaussian(x, mu, sigma):
     """
@@ -99,13 +103,28 @@ def G_bremsstrahlung(x,E0,params_dict,*,elements_dict = {}):
     B = np.vstack((B0,B1)).T
     
     return B
-    
-def elts_dict_from_P (part_P,elements_list) : 
-    flat_P = np.sum(part_P,axis = 1)/np.sum(part_P)
+
+@number_to_symbol_list    
+def elts_dict_from_P (part_P,*,elements = []) : 
+    norm_P = concentrations_from_P(part_P= part_P, elements= elements)
     elements_dict = {}
-    for i,elt in enumerate(elements_list) :
-        elements_dict[elt] = flat_P[i] 
+    with open(SYMBOLS_PERIODIC_TABLE,"r") as f : 
+        SPT = json.load(f)["table"]
+    for i,elt in enumerate(elements) :
+        elements_dict[elt] = norm_P[i] * SPT[elt]["atomic_mass"]
     return elements_dict
+
+@number_to_symbol_list
+def concentrations_from_P (part_P,*, elements = []) : 
+    avg_P = np.sum(part_P,axis = 1)/part_P.shape[1]
+    weighted_P = np.zeros((part_P.shape[0],))
+    with open(SYMBOLS_PERIODIC_TABLE,"r") as f : 
+        SPT = json.load(f)["table"]
+    for i,elt in enumerate(elements) :
+        weighted_P[i] = avg_P[i]*SPT[elt]["atomic_mass"]
+    norm_P = weighted_P / np.sum(weighted_P)
+    return norm_P
+
 
 def elts_dict_from_dict_list (dict_list) : 
     unique_elts_dict = sum((Counter(x) for x in dict_list),Counter())
@@ -115,7 +134,7 @@ def elts_dict_from_dict_list (dict_list) :
     return unique_elts_dict
 
 def update_bremsstrahlung (G,part_P,model_parameters,elements_list, norm = True) : 
-    elts = elts_dict_from_P(part_P,elements_list)
+    elts = elts_dict_from_P(part_P,elements = elements_list)
     model = e.EDXS(**model_parameters)
     B = G_bremsstrahlung(model.x,model.E0,model.params_dict,elements_dict=elts)
     new_G = G.copy()

snmfem/models/absorption_edxs.py
@@ -10,12 +10,14 @@ def absorption_coefficient (x,atomic_fraction = False,*,elements_dict = {"Si" : 
     mu = np.zeros_like(x)
     if atomic_fraction : 
         elements_dict = atomic_to_weight_dict(elements_dict = elements_dict)
+
+    sum_elts = sum(elements_dict.values())
     
     for key in elements_dict.keys() : 
         x_db = HSPY_MAC[key]["energies (keV)"]
         y_db = HSPY_MAC[key]["mass_absorption_coefficient (cm2/g)"]
         interp_func = interp1d(x_db,y_db,kind="cubic")
-        mu += elements_dict[key]*interp_func(x)
+        mu += elements_dict[key]*interp_func(x)/sum_elts
 
     if len(elements_dict.keys()) == 0 :
         mu = 1 / np.power(x,3)

snmfem/tests/test_EDXS.py
@@ -70,13 +70,28 @@ def test_absorption () :
     np.testing.assert_array_less(1e-30,abs_corr)
     np.testing.assert_array_less(abs_corr,1.0)
 
-def test_elts_dict_from_P () : 
+def test_concentrations_from_P () : 
+    elements_mass = [28.085,40.0784,15.999,30.9737619985,12.011]
     part_P = np.random.rand(5,3)
-    norm_P = np.sum(part_P,axis=1)/np.sum(part_P)
+    avg_P = part_P.sum(axis=1)/3
+    weighted_P= [avg_P[i]*elt_mass for i,elt_mass in enumerate(elements_mass)]
+    norm_P = weighted_P/np.sum(weighted_P)
     elements_list = ["Si","Ca", "O", "P", "C"]
-    result = ef.elts_dict_from_P (part_P,elements_list) 
+    
+    result = ef.concentrations_from_P (part_P,elements = elements_list) 
 
-    assert result == {"Si" : norm_P[0], "Ca" : norm_P[1], "O" : norm_P[2], "P" : norm_P[3], "C" : norm_P[4] }
+    np.testing.assert_allclose(result, norm_P)
+
+def test_elts_dict_from_P () : 
+    elements_mass = [28.085,40.0784,15.999,30.9737619985,12.011]
+    part_P = np.random.rand(5,3)
+    avg_P = part_P.sum(axis=1)/3
+    weighted_P= [avg_P[i]*elt_mass for i,elt_mass in enumerate(elements_mass)]
+    norm_P = weighted_P/np.sum(weighted_P)
+    elements_list = ["Si","Ca", "O", "P", "C"]
+
+    result = ef.elts_dict_from_P(part_P, elements = elements_list)
+    assert result == {"Si" : norm_P[0]*elements_mass[0],"Ca" : norm_P[1]*elements_mass[1],"O" : norm_P[2]*elements_mass[2],"P" : norm_P[3]*elements_mass[3],"C" : norm_P[4]*elements_mass[4]}
 
 def test_elts_dict_from_dict_list () : 
     dict_list = [{"chou" : 1, "carottes" : 2, "navet" : 3}, {"chou" : 2, "oignons" : 3, "navet" : 3}, {"orange" : 6, "citron" : 4, "poireau" : 8}]
@@ -87,13 +102,8 @@ def test_elts_dict_from_dict_list () :
 def test_continuum_xrays () : 
     E0 = 200
     b0 = np.random.uniform(0.0,50.0)
-    b1 = np.random.uniform(-50.0, 50.0)
-    if b1 >= (-b0/E0) : 
-        cont_x = ef.continuum_xrays(x,model_parameters["params_dict"],b0= b0, b1 = b1, E0 = E0,elements_dict = {"Si" : 1.0, "C" : 0.2, "Bi" : 3.2})
-
-    else : 
-        b1 = -b0/E0
-        cont_x = ef.continuum_xrays(x,model_parameters["params_dict"],b0= b0, b1 = b1, E0 = E0,elements_dict = {"Si" : 1.0, "C" : 0.2, "Bi" : 3.2})
+    b1 = np.random.uniform(0.0, 50.0)
+    cont_x = ef.continuum_xrays(x,model_parameters["params_dict"],b0= b0, b1 = b1, E0 = E0,elements_dict = {"Si" : 1.0, "C" : 0.2, "Bi" : 3.2})
     
     np.testing.assert_array_less(1e-30,cont_x)
 

snmfem/tests/test_estimators.py
@@ -95,7 +95,7 @@ def test_generate_one_sample():
     G, P, A, D, w, X, Xdot, N = generate_one_sample()
     np.testing.assert_allclose(G @ P , D, atol=1e-3)
     np.testing.assert_allclose( N * D @ np.diag(w) @ A , Xdot)
-    np.testing.assert_allclose(N * G @ P @ np.diag(w) @ A , Xdot, atol=5e-2)
+    np.testing.assert_allclose(N * G @ P @ np.diag(w) @ A , Xdot, atol=1e-1)
 
 def test_NMF_scikit () : 
     estimator = NMF(n_components= 5,max_iter=200,force_simplex = True,mu = 1.0, epsilon_reg = 1.0,hspy_comp = False)
@@ -108,19 +108,19 @@ def test_general():
 
     estimator = NMF(G=G,n_components= 2,max_iter=200,force_simplex = True,mu = 0, epsilon_reg = 1, hspy_comp = False)
     D2 = estimator.fit_transform(A=A, X=Xdot)
-    np.testing.assert_allclose(N*D@np.diag(w), D2, atol=5e-2)
+    np.testing.assert_allclose(N*D@np.diag(w), D2, atol=1e-1)
 
     estimator = NMF(n_components= 2,max_iter=200,force_simplex = True,mu = 0, epsilon_reg = 1, hspy_comp = False)
     D2 = estimator.fit_transform(A=A, X=Xdot)
-    np.testing.assert_allclose(N*D@np.diag(w), D2, atol=5e-2)
+    np.testing.assert_allclose(N*D@np.diag(w), D2, atol=1e-1)
 
     estimator = NMF(G=G,n_components= 2,max_iter=200,force_simplex = False,mu = 0, epsilon_reg = 1, hspy_comp = False)
     D2 = estimator.fit_transform( P=P@np.diag(w), X=Xdot)
-    np.testing.assert_allclose(N*D@np.diag(w), D2, atol=5e-2)
+    np.testing.assert_allclose(N*D@np.diag(w), D2, atol=1e-1)
 
     estimator = NMF(G =G, n_components= 2,max_iter=200,force_simplex = True,mu = 0, epsilon_reg = 1, hspy_comp = False)
     D2 = estimator.fit_transform(P=N*P@np.diag(w), X=Xdot)
-    np.testing.assert_allclose(N*D@np.diag(w), D2, atol=5e-2)
+    np.testing.assert_allclose(N*D@np.diag(w), D2, atol=1e-1)
 
     estimator = NMF(G=G, n_components= 2,max_iter=200,force_simplex = True,mu = 0, epsilon_reg = 1, hspy_comp = False)
     estimator.fit_transform(X=Xdot)