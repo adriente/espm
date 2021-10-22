47779dda7eab8a8c778d3506a2e673175f6a9b98
adriente <adrien.teurtrie@epfl.ch>
2021-10-22 17:56:07 +0200

    fixed P fixed

--- snmfem/estimators/smooth_nmf.py
+++ snmfem/estimators/smooth_nmf.py
@@ -58,7 +58,7 @@ class SmoothNMF(NMF):
                 self.gamma.append(self.sigmaL_ )
         else:
             A = multiplicative_step_a(self.X_, self.G_, P, A, force_simplex=self.force_simplex, mu=self.mu, eps=self.log_shift, epsilon_reg=self.epsilon_reg, safe=self.debug, dicotomy_tol=self.dicotomy_tol, lambda_L=self.lambda_L, L=self.L_, l2=self.l2, fixed_A_inds=self.fixed_A_inds)
-            P = multiplicative_step_p(self.X_, self.G_, P, A, eps=self.log_shift, safe=self.debug, l2=self.l2)
+            P = multiplicative_step_p(self.X_, self.G_, P, A, eps=self.log_shift, safe=self.debug, l2=self.l2,fixed_P=self.fixed_P)
 
         if callable(self.G) : 
             self.G_ = self.G(part_P = P[:-2,:],G = self.G_)
