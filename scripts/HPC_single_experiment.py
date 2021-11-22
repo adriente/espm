eccbb77a4dbc3f4ff7224202ac8a39af3d4ccf2d
adriente <adrien.teurtrie@epfl.ch>
2021-11-22 10:40:38 +0100

    remove add constant

--- scripts/HPC_single_experiment.py
+++ scripts/HPC_single_experiment.py
@@ -12,9 +12,6 @@ def run_single_exp(pos_dict,est_dict,eval_dict) :
         with open(eval_dict["fixed_P_json"],"r") as f :
             json_dict = json.load(f)
         spim, estim = e.quick_load(experiment = exp,sim = eval_dict["simulated"], P_dict = json_dict)
-    if np.min(spim.data.sum(axis=0)) == 0.0 : 
-        spim.add_constant(1e-12)
-        print("0 counts detected, adding a small constant")
     metrics, mat_tuple, losses = e.run_experiment(spim, estim, exp, sim=eval_dict["simulated"])
     e.store_in_file(eval_dict["output_file"], metrics, mat_tuple, losses)
 
